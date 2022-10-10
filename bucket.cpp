f68087c08987ec1fcf3c719600f116410d855605
BonmooKoo <rnqhsan9812@gmail.com>
2022-10-10 18:43:58 +0900

    TODO:insert

--- bucket.cpp
+++ bucket.cpp
@@ -10,5 +10,17 @@ class Bucket
     private:
         char array[BUCKET_SIZE];
         
-
+    public:
+        void insert(char* key,char* value);
+        void update(char* key,char* value);
+        void remove(char* key);
+        
 };
+
+void Bucket::insert(char* key,char* value){
+
+}
+
+int main(){
+    
+}
