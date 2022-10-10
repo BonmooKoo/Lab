75e18eea66b6949330bbb7136df0bc0e998edccc
BonmooKoo <rnqhsan9812@gmail.com>
2022-10-10 17:50:21 +0900

    doit

---
+++ bucket.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#define BUCKET_SIZE 1024;
+#define KEY_SIZE 16;
+#define VALUE_SIZE 16;
+using namespace std;
+
+class Bucket
+{
+    private:
+        char array[BUCKET_SIZE];
+        
+
+};
