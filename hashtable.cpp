93ed3f2a0d4551f2811dcc6debbedd95fa1449c0
BonmooKoo <rnqhsan9812@gmail.com>
2022-10-11 14:32:20 +0900

    UPDATE:BucketandTable

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <cstring>
+#define TABLE_SIZE 1024*1024
+
+using namespace std;
 
 class hashtable{
     private:
-
+        int16_t global_depth;
     public :
-    void insert()
-}
+        void insert();
+        void remove();
+        void search(char* key, char* value);
+};
