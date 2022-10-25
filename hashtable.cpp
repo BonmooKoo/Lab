d318aa75ac17434a9d818eec43f86bf6e9396f3e
BonmooKoo <rnqhsan9812@gmail.com>
2022-10-25 17:09:37 +0900

    TODO:Split()

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,11 +1,47 @@
 #include "header.h"
 
-int hashtable::getSize(){
-    return sizeof(table);
+int hashtable::getSizeTable(){
+    return TABLE_SIZE/BUCKET_SIZE-sizeof(int8_t);
 }
 void hashtable::initialize(){
     global_depth=1;
-    for(int i=0;i<getSize();i++){
+}
+int hashtable::hashingKey(char* key){
+    //간단한 hash func
+    unsigned long i = 0;
+    for (int j=0; key[j]; j++)
+        i += key[j];
+    return i % getSizeTable();
+    return;
+}
+Bucket* hashtable::split(Bucket* splitBucket){
+    Bucket* newBucket=new Bucket();
+    for(int i=0;i<splitBucket->getSize();i++){
         
+        
+        newBucket->insert();
+    }
+    //
+    addLocaldepth();
+    return newBucket;
+}
+int hashtable::insertKV(char* key,char* value ){
+    //삽입
+    int index=hashingKey(key);
+    int rtnBucket=0;
+    if((rtnBucket=table[index]->insert(key,value))==-1){
+        //중복이 있거나 삽입에 실패
+        return -1;
+    }
+    else{
+        return rtnBucket;
     }
 }
+char* hashtable::searchKV(char* key){
+    //
+    int index=hashingKey(key);
+    char* rtnValue;
+    rtnValue=table[index]->lookup(key);
+    return NULL;
+}
+
--- header.h
+++ header.h
@@ -20,28 +20,36 @@ class Bucket
         //bool bitmap[BUCKET_SIZE/(KEY_SIZE+VALUE_SIZE)-2];//62
         char array[BUCKET_SIZE-sizeof(int8_t)-sizeof(bitmap)]; //60 * 16 B
     public:
-        int insert(char* key,char* value);
+        Bucket();
+        int8_t getLocaldepth();
+        void addLocaldepth();
+        int insert(char* key,char* value); //-1은 중복 -2는 bucketfull
         void update(char* key,char* value);
         bool remove(char* key);
         char* lookup(char* key);
         void initialize();
         int getSize();
+        Bucket* split(char* hashkey);
 };
 
 class hashtable{
     private:
     //TABLE_SIZE/BUCKET_SIZE
-        int16_t global_depth;
+        int8_t global_depth;
         //char* cache[CACHE_SIZE]
         //Bucket* table[TABLE_SIZE/BUCKET_SIZE-sizeof(int16_t)-sizeof(cache)];//1024
         Bucket* table[TABLE_SIZE/BUCKET_SIZE-sizeof(int16_t)];//1024
     public:
-        void insert();
-        void remove();
-        void initialize();
-        void search(char* key, char* value);
-        int getSize();
 
+        int insertKV(char* key, char* value);
+        void doubleTable();
+        void removeKV(char* key);
+        void initialize();
+        char* searchKV(char* key);
+        int getSizeTable();
+        int hashingKey(char *key);
+        Bucket* split(Bucket* splitBucket);
+    
 };
 
 #endif 
